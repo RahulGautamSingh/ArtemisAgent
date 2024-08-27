73d7e0b50fa7c806add9b8284d84d07ba86c4605
Jordan Longstaff <jrdnlngstff@gmail.com>
2024-08-27 10:29:15 -0400

    Add more assertions to HelpFragmentTest

--- app/src/androidTest/kotlin/artemis/agent/HelpFragmentTest.kt
+++ app/src/androidTest/kotlin/artemis/agent/HelpFragmentTest.kt
@@ -3,6 +3,8 @@ package artemis.agent
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
+import com.adevinta.android.barista.assertion.BaristaListAssertions.assertDisplayedAtPosition
+import com.adevinta.android.barista.assertion.BaristaRecyclerViewAssertions.assertRecyclerViewItemCount
 import com.adevinta.android.barista.assertion.BaristaVisibilityAssertions.assertDisplayed
 import com.adevinta.android.barista.assertion.BaristaVisibilityAssertions.assertNotDisplayed
 import com.adevinta.android.barista.interaction.BaristaClickInteractions.clickOn
@@ -13,11 +15,7 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 @LargeTest
 class HelpFragmentTest {
-    @get:Rule
-    val activityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
-
-    @Test
-    fun menuOptionsTest() {
+    private companion object {
         val stringResources = intArrayOf(
             R.string.help_topics_getting_started,
             R.string.help_topics_basics,
@@ -29,22 +27,37 @@ class HelpFragmentTest {
             R.string.help_topics_biomechs,
             R.string.help_topics_about,
         )
+    }
 
-        clickOn(R.id.helpPageButton)
+    @get:Rule
+    val activityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
 
+    private fun assertHelpMenuDisplayed() {
         assertNotDisplayed(R.id.helpTopicTitle)
         assertNotDisplayed(R.id.backButton)
+        assertDisplayed(R.id.helpTopicContent)
+        assertRecyclerViewItemCount(R.id.helpTopicContent, stringResources.size)
+
+        stringResources.forEachIndexed { index, res ->
+            assertDisplayedAtPosition(R.id.helpTopicContent, index, res)
+        }
+    }
+
+    @Test
+    fun menuOptionsTest() {
+        clickOn(R.id.helpPageButton)
+        assertHelpMenuDisplayed()
 
         stringResources.forEach { stringRes ->
             clickOn(stringRes)
 
             assertDisplayed(R.id.helpTopicTitle, stringRes)
             assertDisplayed(R.id.backButton)
+            assertDisplayed(R.id.helpTopicContent)
 
             clickOn(R.id.backButton)
 
-            assertNotDisplayed(R.id.helpTopicTitle)
-            assertNotDisplayed(R.id.backButton)
+            assertHelpMenuDisplayed()
         }
     }
 }
