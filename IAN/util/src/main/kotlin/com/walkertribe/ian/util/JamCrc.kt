60fa7f6f2b17a7df37907ae3298eb94ef5c4c96d
Jordan Longstaff <jrdnlngstff@gmail.com>
2024-09-10 22:49:46 -0400

    Resolve magic numbers in JamCrc

--- IAN/util/src/main/kotlin/com/walkertribe/ian/util/JamCrc.kt
+++ IAN/util/src/main/kotlin/com/walkertribe/ian/util/JamCrc.kt
@@ -4,36 +4,41 @@ import korlibs.io.lang.UTF8
 import korlibs.io.lang.toByteArray
 
 /**
- * An implementation of the JamCRC algorithm. These checksums are used to allow
- * Artemis to send an int instead of a full string.
+ * An implementation of the JamCRC algorithm. These checksums are used to allow Artemis to send an
+ * integer instead of a full string.
  * @author rjwut
  */
 object JamCrc {
     private const val INITIAL_VALUE = 0.inv()
     private const val POLYNOMIAL = 0x04c11db7
-    private val TABLE = IntArray(256) {
-        var refl = reflect(it, 8.toByte()) shl 24
-        repeat(8) {
-            refl = refl shl 1 xor if (refl and (1 shl 31) == 0) 0 else POLYNOMIAL
+    private const val TABLE_SIZE = 256
+    private const val BYTE_MASK = 0xff
+    private const val INT_MSB = 1 shl 31
+    private const val TOP_BYTE_SHIFT = Int.SIZE_BITS - Byte.SIZE_BITS
+
+    private val TABLE = IntArray(TABLE_SIZE) {
+        var refl = reflect(it, Byte.SIZE_BITS) shl TOP_BYTE_SHIFT
+        repeat(Byte.SIZE_BITS) {
+            refl = refl shl 1 xor if (refl and INT_MSB == 0) 0 else POLYNOMIAL
         }
-        reflect(refl, 32.toByte())
+        reflect(refl, Int.SIZE_BITS)
     }
 
     /**
      * Computes a checksum for the given byte array.
      */
-    fun compute(bytes: ByteArray): Int = bytes.fold(INITIAL_VALUE) { crc, b ->
-        crc ushr 8 xor TABLE[crc and 0xff xor (b.toInt() and 0xff)]
+    private fun compute(bytes: ByteArray): Int = bytes.fold(INITIAL_VALUE) { crc, b ->
+        crc ushr Byte.SIZE_BITS xor TABLE[crc and BYTE_MASK xor (b.toInt() and BYTE_MASK)]
     }
 
     /**
-     * Computes a checksum for the UTF-8 representation of the given string.
-     * Note that even though strings are transmitted in UTF-16LE in the Artemis
-     * protocol, checksums are computed in UTF-8. Weirdness.
+     * Computes a checksum for the UTF-8 representation of the given string. Note that even though
+     * strings are transmitted in UTF-16LE in the Artemis protocol, checksums are computed in UTF-8.
+     * Weirdness.
      */
     fun compute(str: String): Int = compute(str.toByteArray(UTF8))
 
-    private fun reflect(ref: Int, ch: Byte): Int = (0 until ch).map {
+    private fun reflect(ref: Int, ch: Int): Int = BooleanArray(ch) {
         ref and (1 shl it) != 0
     }.fold(0) { value, bit ->
         value shl 1 or if (bit) 1 else 0
